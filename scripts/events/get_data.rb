849f241e6128519e57e3f41b2b248ea23e8548eb
ydx <x6doooo@gmail.com>
2013-03-22 16:42:07 +0800

    获取地区活动列表
    主要关注地理字段

--- scripts/events/get_data.rb
+++ scripts/events/get_data.rb
@@ -11,6 +11,8 @@ require '../modules/api_getter'
 # google api
 # http://maps.googleapis.com/maps/api/geocode/json?address=%E5%A4%A9%E6%B4%A5&sensor=false
 
+$has_it = []
+
 #获取数据
 class Worker < API_getter
   attr_accessor :json, :sql
@@ -21,37 +23,26 @@ class Worker < API_getter
   end
   #将数据插入数据库
   def insert_to_db
-
-    puts @json
-=begin
-    @json["onlines"].each do |line|
-      $has_it.push(line["id"])
-      keys = []
+    fields = ["participant_count", "image", "begin_time", "geo", "title", "wisher_count", "address"]
+    @json["events"].each do |line|
+      $has_it.push line["id"]
       values = []
-      line.each do |key, value|
-        if key == 'tags'
-          value = value.join('|')
-        end
-        keys.push('`' + key.to_s + '`')
-        values.push("'" + value.to_s.gsub(/\'/, '') + "'")
+      fields.each do |f|
+        values.push "'#{line[f]}'"
       end
-      keys = keys.join(',')
-      values = values.join(',')
-      odr = "INSERT into actives_data (#{keys}) VALUES (#{values})"
-      puts "log: #{line["id"]} done."
-      #@sql.query(odr)
+      odr = "INSERT into events_bj (#{fields.join(',')}) VALUES (#{values.join(',')})"
+      @sql odr
     end
-=end
     puts "log: #{$has_it.length} actives done."
   end
 end
 
-looper = API_Looper.new(100, 0, 100, 4)
+looper = API_Looper.new(3717, 0, 100, 4)
 
 worker = Worker.new('https://api.douban.com/v2/event/list')
 
 looper.loop_get do |start, count, sleeptime|
-  worker.get_active_info(start, count)
+  worker.get_event_info(start, count)
   worker.insert_to_db
   sleep sleeptime
 end
