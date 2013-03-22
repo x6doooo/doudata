823393a2ff671471296b89780a01f63ad2a81cab
ydx <x6doooo@gmail.com>
2013-03-22 15:24:25 +0800

    新增同城类目
    备用

---
+++ scripts/events/get_data.rb
@@ -0,0 +1,6 @@
+
+# douban api
+# https://api.douban.com/v2/event/list?loc=108288&start=0&count=100
+
+# google api
+# http://maps.googleapis.com/maps/api/geocode/json?address=%E5%A4%A9%E6%B4%A5&sensor=false
