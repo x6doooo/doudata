ff4d525d3d79ecad44ca14bb8ac7cd8ff26f6ce0
ydx <x6doooo@gmail.com>
2013-03-23 21:43:15 +0800

    修正展现events形式
    取消时间轴计划

--- statics/js/app/app.js
+++ statics/js/app/app.js
@@ -12,19 +12,9 @@ function EventsMapCtrl($scope, $http, $routeParams){
         bj: [39.93, 116.40]
       },
       eventsData = new google.maps.MVCArray(),
-      latlng, myOptions, map, styles, heatmap;
+      latlng, myOptions, map, styles, heatmap, tem;
 
   initialize(pos[locId]);
-
-  $http.get('/doudata/json/events/'+ locId +'_heat.json').success(function(data){
-    $scope.events = data;
-    data.forEach(function(val, idx, arr){
-      val[0] = val[0].split(' ');
-      for(var i = 0, len = val[3] * 1; i < len; i++){
-        eventsData.push(new google.maps.LatLng(val[0][0], val[0][1]));
-      }
-    });
-  });
 
   function initialize(pos) {
     latlng = new google.maps.LatLng(pos[0], pos[1]);
@@ -46,17 +36,27 @@ function EventsMapCtrl($scope, $http, $routeParams){
     heatmap = new google.maps.visualization.HeatmapLayer({
       map: map,
       data: eventsData,
-      radius: 20,
+      radius: 10,
       dissipate: false,
-      maxIntensity: 8,
+      maxIntensity: 8/*,
       gradient: [
         'rgba(0, 0, 0, 0)',
         'rgba(255, 255, 0, 0.50)',
         'rgba(255, 128, 0, 1.0)'
-      ]
+      ]*/
     });
     google.maps.event.addListener(map, 'tilesloaded', function() {
-      unlock = true;
+      $http.get('/doudata/json/events/'+ locId +'_heat.json').success(function(data){
+        $scope.events = data;
+        data.forEach(function(val, idx, arr){
+          val[0] = val[0].split(' ');
+          tem = val[3] * 1;
+          while(tem >= 0){
+            eventsData.push(new google.maps.LatLng(val[0][0], val[0][1]));
+            tem -= 1;
+          }
+        });
+      });
     });
     map.setOptions({styles: styles});
   }
